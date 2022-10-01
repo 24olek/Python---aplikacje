0a2c6e159a8fbed85204150a69af8fc887596108
24olek <102329809+24olek@users.noreply.github.com>
2022-10-01 17:38:06 +0200

    Update p@ssw.rd.py

--- p@ssw.rd.py
+++ p@ssw.rd.py
@@ -1,8 +1,11 @@
 import random
 marks = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[]\{}|;':\",./<>?"
 password = ""
-for x in range(15):
-    i = random.randrange(len(marks))
-    password += marks[i]
-
-print(password)
+inp = ""
+while inp != "exit":
+    for x in range(15):
+        i = random.randrange(len(marks))
+        password += marks[i]
+    print(password)
+    password = ""
+    inp = input("For exit, write 'exit'").lower()
